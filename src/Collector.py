7aa6e700094ea6d952b8316b5d23d479abc99401
ywcb00 <ywcb00@ywcb.org>
2023-05-25 12:04:04 +0200

    feat(Collector.py): implement the update routine

--- src/Collector.py
+++ src/Collector.py
@@ -32,6 +32,17 @@ class Collector:
         row_df = pd.DataFrame([stat_row])
         self.collected_stats = pd.concat([self.collected_stats, row_df])
 
+    def updateStats(self, index, url, title, content):
+        stat_row = {'url': url, 'title': title}
+        for stat in self.statlist:
+            stat_colnames = [cn for cn in self.collected_stats.columns if cn.startswith(stat.SHORT_NAME)]
+            if(not self.collected_stats.loc[index, stat_colnames].isnull().values.any()):
+                continue # no need to update existing statistics
+            stat_arr = stat.collect(title, content)
+            stat_dict = self.getStatisticalValuesDict(stat_arr, stat.SHORT_NAME)
+            print("=====", stat_dict)
+            stat_row = {**stat_row, **stat_dict}
+        self.collected_stats.loc[index, stat_row.keys()] = stat_row.values()
 
     def getStatisticalValuesDict(self, stat_arr, short_name):
         stat_dict = {}
@@ -59,5 +70,22 @@ class Collector:
                     continue # article is not between 50 and 100 sentences
                 self.collectStats(url, title, content)
             self.storeCollection()
-            
 
+    def updateRoutine(self):
+        for newsname, config in crawldata.items():
+            print(f'Updating articles from {newsname}')
+            for idx, cs in self.collected_stats.iterrows():
+                url = cs.url
+                if(not cs.url.startswith(config['url-prefix'])):
+                    continue # only update entries for the current configuration
+                if(not cs.isnull().values.any()):
+                    continue # no need to complete full rows
+                print(f'=== ({idx+1}/{self.collected_stats.shape[0]}) {url}')
+                title = crawler.scrapeHeading(url, config)
+                content = crawler.scrapeArticle(url, config)
+                if (not title) or (not content):
+                    continue # we cannot collect articles without title or content
+                if (not numSentencesBetween(content, MIN_SENTENCES, MAX_SENTENCES)):
+                    continue # article is not between 50 and 100 sentences
+                self.updateStats(idx, url, title, content)
+            self.storeCollection()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 from Collector import Collector
+import getopt
+import sys
 
-def main():
+def main(argv):
     collector = Collector()
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "hu:", ["help", "update"])
+    except getopt.GetoptError:
+        print("Wrong usage.")
+        print("Usage:", argv[0], "[--update]")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-u", "--update"):
+            collector.updateRoutine()
+            sys.exit(0)
+
     collector.collectionRoutine()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
